4cde6a8801fa20ac0cdf6d72fa5bc7bd746b722b
mtgd24 <hoangcaophong2001@gmail.com>
2025-05-09 16:52:37 +0800

    Initial Commit

---
+++ Home security 5/Home_security_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Home_security_5App.swift
+//  Home security 5
+//
+//  Created by MTGD on 2025/5/9.
+//
+
+import SwiftUI
+
+@main
+struct Home_security_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
