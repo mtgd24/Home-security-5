15bcca726940aba3dd40d091a961c2e0e68ff512
mtgd24 <hoangcaophong2001@gmail.com>
2025-06-05 16:05:33 +0800

    aaaa

---
+++ Home security 5/DevicesView.swift
@@ -0,0 +1,18 @@
+//
+//  DevicesView.swift
+//  Home security 5
+//
+//  Created by MTGD on 2025/6/5.
+//
+
+import SwiftUI
+
+struct DevicesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DevicesView()
+}
---
+++ Home security 5/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Home security 5
+//
+//  Created by MTGD on 2025/6/5.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
